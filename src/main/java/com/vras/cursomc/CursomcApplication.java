d86cadc14760c5981388a1b190168747db095568
Victor Assunção <victor.assuncao@55solucoes.com>
2018-11-19 20:45:36 -0300

    Finalizado Pedido e ItemPedido

--- src/main/java/com/vras/cursomc/CursomcApplication.java
+++ src/main/java/com/vras/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.vras.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,22 @@ import com.vras.cursomc.domain.Cidade;
 import com.vras.cursomc.domain.Cliente;
 import com.vras.cursomc.domain.Endereco;
 import com.vras.cursomc.domain.Estado;
+import com.vras.cursomc.domain.ItemPedido;
+import com.vras.cursomc.domain.Pagamento;
+import com.vras.cursomc.domain.PagamentoComBoleto;
+import com.vras.cursomc.domain.PagamentoComCartao;
+import com.vras.cursomc.domain.Pedido;
 import com.vras.cursomc.domain.Produto;
+import com.vras.cursomc.domain.enums.EstadoPagamento;
 import com.vras.cursomc.domain.enums.TipoCliente;
 import com.vras.cursomc.repositories.CategoriaRepository;
 import com.vras.cursomc.repositories.CidadeRepository;
 import com.vras.cursomc.repositories.ClienteRepository;
 import com.vras.cursomc.repositories.EnderecoRepository;
 import com.vras.cursomc.repositories.EstadoRepository;
+import com.vras.cursomc.repositories.ItemPedidoRepository;
+import com.vras.cursomc.repositories.PagamentoRepository;
+import com.vras.cursomc.repositories.PedidoRepository;
 import com.vras.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +51,15 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,5 +108,34 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.save(cli1);
 		enderecoRepository.saveAll(Arrays.asList(ed1,ed2));
+		
+		Pedido ped1 = new Pedido(null, new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("30/09/2017 10:32"), cli1, ed1);
+		Pedido ped2 = new Pedido(null, new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("10/10/2017 19:35"), cli1, ed2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, 
+				new SimpleDateFormat("dd/MM/yyyy HH:mm").parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedido().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+		
 	}
 }
